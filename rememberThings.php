0bee0acf8c61bee312d03338eaa0d0d0f63479b6
nicktamburro <nicholastamburro@gmail.com>
2018-10-29 12:50:53 -0700

    remembering old friend php

---
+++ rememberThings.php
@@ -0,0 +1,13 @@
+<?php
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo "This is where we say $txt"
+echo "Now give us " $x x $y " dollars!"
+
+echo "We could also do it like this: " . $txt . "okay?"
+
+
+?>
