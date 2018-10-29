34bbcc39f0ff544fbf7d7dfe6e68c858fe5d4b93
nicktamburro <nicholastamburro@gmail.com>
2018-10-29 12:55:38 -0700

    more refresh

--- rememberThings.php
+++ rememberThings.php
@@ -4,10 +4,21 @@ $txt = "Hello world!";
 $x = 5;
 $y = 10.5;
 
-echo "This is where we say $txt"
-echo "Now give us " $x x $y " dollars!"
+echo "This is where we say $txt";
+echo "Now give us " $x x $y " dollars!";
 
-echo "We could also do it like this: " . $txt . "okay?"
+echo "We could also do it like this: " . $txt . "okay?";
 
+//but when we do this:
+function getMoney(){
+	//this won't work, functions CAN'T see global variables from their scope
+	echo "We want $x dollars, baby!"
+}
+
+//this will fail
+getMoney()
+
+//but this is all good:
+echo "And now give us $y more dollars";
 
 ?>
