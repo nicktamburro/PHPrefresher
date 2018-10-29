05dcf83e2477a24f915f0ff303e7ae5087bec8ec
nicktamburro <nicholastamburro@gmail.com>
2018-10-29 13:02:20 -0700

    playing with php functions

--- rememberThings.php
+++ rememberThings.php
@@ -9,14 +9,22 @@ echo "Now give us " $x x $y " dollars!";
 
 echo "We could also do it like this: " . $txt . "okay?";
 
+
 //but when we do this:
 function getMoney(){
 	//this won't work, functions CAN'T see global variables from their scope
 	echo "We want $x dollars, baby!"
 }
-
 //this will fail
-getMoney()
+getMoney();
+
+//but this one will work
+function tryAgain(){
+	$x = 100;
+	echo "We want $x dollars, baby!"
+}
+//this will work!
+tryAgain();
 
 //but this is all good:
 echo "And now give us $y more dollars";
